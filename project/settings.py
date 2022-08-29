3225e7c83fa946a8467abc18844f0208b6ebedbd
Ahmed-Hashim <ah.abolaban@gmail.com>
2022-08-29 22:58:32 +0200

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -133,13 +133,14 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = 'static/'
-STATICFILES_DIRS= [
-    BASE_DIR / 'static', 
-]
-#STATIC_ROOT=BASE_DIR / 'staticfiles'
-MEDIA_URL='media/'
-STATIC_ROOT = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'posts/static'),
+    os.path.join(BASE_DIR, 'members/static'),
+)
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
